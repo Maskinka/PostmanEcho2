552ea11894c21a34bb9a7fd0158f762a30955a48
Kozhevnikova Maria <maskinka@bk.ru>
2022-07-22 22:54:09 +0700

    Add test + tests failed

--- src/test/java/Postman.java
+++ src/test/java/Postman.java
@@ -17,7 +17,21 @@ public class Postman {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("info post"))
+                .body("data", equalTo("info"))
+        ;
+    }
+
+    @Test
+    void requestPostmanKirill(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("информация")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("что то"))
         ;
     }
 }
